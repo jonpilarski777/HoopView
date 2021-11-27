0507672b0b164ffc167f0969545fa0e975117b83
Jon <jonpilarski927@gmail.com>
2021-11-27 17:00:44 -0600

    updated migration tables

--- db/seeds.rb
+++ db/seeds.rb
@@ -63,28 +63,28 @@ favorite_teams = FavoriteTeam.create(teams_list)
 
 
 
-puts "Seeding Schedules"
-schedules = Schedule.create([
-    {date: Time.utc(2021, 12, 10, 11),
-    venue: "Mackey Arena",
-    home: 1,
-    favorite_team_id:  
+# puts "Seeding Schedules"
+# schedules = Schedule.create([
+#     {date: Time.utc(2021, 12, 10, 11),
+#     venue: "Mackey Arena",
+#     home: 1,
+#     favorite_team_id:  
 
     
     
-    }
+#     }
 
 
 
 
-])
+# # ])
 
-puts "Seeding user daily games"
-
-
-puts "Seeding daily games"
+# puts "Seeding user daily games"
 
 
-puts "Seeding Team Stats"
+# puts "Seeding daily games"
+
+
+# puts "Seeding Team Stats"
 
 
