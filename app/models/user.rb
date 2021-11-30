7dc74c936143c95d333d9f63d036d0fb8e339e4f
Jon <jonpilarski927@gmail.com>
2021-11-30 13:19:44 -0600

    finished validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,6 @@ class User < ApplicationRecord
     has_secure_password
 
     validates :username, presence: true, uniqueness: true
+    validates :email, uniqueness: true, allow_blank: true
 
 end
--- app/models/user_favorite_team.rb
+++ app/models/user_favorite_team.rb
@@ -1,4 +1,7 @@
 class UserFavoriteTeam < ApplicationRecord
     belongs_to :user
     belongs_to :favorite_team
+
+    validates :favorite_team_id, uniqueness: { scope: [:user_id], message: "Can't select the same favorite team twice" }
+    validates :team, inclusion: { in: ->(team) { FavoriteTeam.map {|a| a.team_name}}}
 end
