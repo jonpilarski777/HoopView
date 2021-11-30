f0a4e9e53341a255a29a3fc6b4338312f36d7250
Jon <jonpilarski927@gmail.com>
2021-11-30 08:55:37 -0600

    Added user/sessions/app controllers (commented out now for testing purposes)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,34 @@
 class ApplicationController < ActionController::API
     include ActionController::Cookies
+    # rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
+    # before_action :confirm_authentication
+  
 
     def hello_world
         session[:count] = (session[:count] || 0) + 1
         render json: { count: session[:count] }
     end
 
+    # private
+  
+    # def current_user
+    #   @current_user ||= User.find_by(id: session[:user_id])
+    # end
+  
+    # def logged_in?
+    #   current_user (put !! in front later)
+    # end
+  
+    # def confirm_authentication
+    #   render json: { error: "You must be logged in to do that." }, status: :unauthorized unless logged_in?
+    # end
+  
+    # def not_found(e)
+    #   render json: { error: e.message }, status: :not_found
+    # end
+
+  
 end
 
 
