42a38d9996d66711b93f84af6936a54bbcb83e89
Jon <jonpilarski927@gmail.com>
2021-11-30 17:02:15 -0600

    COMMIT IS FOR REFERENCE THAT YOU COMMENTED OUT ALL AUTHENTICATION ROUTESgit add . !

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,20 +1,20 @@
 class Api::SessionsController < ApplicationController
-    skip_before_action :confirm_authentication
+    # skip_before_action :confirm_authentication
 
-    # post '/login'
-    def create
-      user = User.find_by_username(params[:username])
-      if user&.authenticate(params[:password])
-        session[:user_id] = user.id
-        render json: user, status: :ok
-      else
-        render json: { error: 'invalid credentials' }, status: :unauthorized
-      end
-    end
+    # # post '/login'
+    # def create
+    #   user = User.find_by_username(params[:username])
+    #   if user&.authenticate(params[:password])
+    #     session[:user_id] = user.id
+    #     render json: user, status: :ok
+    #   else
+    #     render json: { error: 'invalid credentials' }, status: :unauthorized
+    #   end
+    # end
   
-    # delete '/logout'
-    def destroy
-      session.delete(:user_id)
-    end
+    # # delete '/logout'
+    # def destroy
+    #   session.delete(:user_id)
+    # end
   
 end
