3b37f7768fae1451c34f42f35520f06aef99fd29
Jon <jonpilarski927@gmail.com>
2021-12-04 20:15:45 -0600

    Added Chart-js, popup, styled components, react-table

--- app/serializers/daily_game_serializer.rb
+++ app/serializers/daily_game_serializer.rb
@@ -1,6 +1,8 @@
 class DailyGameSerializer < ActiveModel::Serializer
   attributes :id, :matchup, :date, :venue, :user_daily_game
+  has_many :favorite_team_details
 
+  
   def user_daily_game 
     UserDailyGame.find_by(user_id: current_user.id, daily_game_id: self.object.id)
   end
