2257c0842758b7c4f170189337acd5011ca9226f
Jon <jonpilarski927@gmail.com>
2021-12-06 10:07:35 -0600

    Added validations for ranking

--- app/models/user_favorite_team.rb
+++ app/models/user_favorite_team.rb
@@ -6,4 +6,6 @@ class UserFavoriteTeam < ApplicationRecord
     # I think it this one WONT have scope!!, just uniqueness by itself
 
     validates :favorite_team_id, presence: true
+    validates :ranking, uniqueness: {scope: [:user_id]}
+    validates :ranking, numericality: {greater_than_or_equal_to: 1, less_than_or_equal_to: 10}
 end
