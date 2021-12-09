7c7a221f7389166c0d773fe1b5e893ebb0a23ba4
Jon <jonpilarski927@gmail.com>
2021-12-09 14:47:11 -0600

    Updated Team Schedule fetch

--- client/src/components/TeamSchedule.js
+++ client/src/components/TeamSchedule.js
@@ -1,23 +1,50 @@
 import React, {useState, useEffect, useCallback} from 'react'
+import {useHistory} from 'react-router-dom'
 
-function TeamSchedule({team}) {
-    const[schedule, setSchedule] = useState([])
+
+
+function TeamSchedule({newteamId, team}) {
+    const[schedule, setSchedule] = useState({})
+    let history = useHistory();
     // const [sortedBy, setSortedBy] = useState(null)
     // const [ascending, setAscending] = useState(true)
 
+
+    console.log("schedule:", schedule)
+
     const fetchTeamSchedule = useCallback(() =>{
-    fetch(`http://site.api.espn.com/apis/site/v2/sports/basketball/mens-college-basketball/teams/2000`)
+    fetch(`http://site.api.espn.com/apis/site/v2/sports/basketball/mens-college-basketball/teams/${team.espn_id}/schedule?season=2022`)
     .then(r => r.json())
     .then(schedule => setSchedule(schedule))})
 
+    console.log("schedule:", schedule)
 
-    useEffect(() => {fetchTeamSchedule() }, [team.espn_id])
+    const backtoGames = () => {
+        history.push("/games")
+    }
 
-    
+    useEffect(() => {fetchTeamSchedule() }, [])
+
+       
+    const showSchedule = (team) => {
+        if (team.id === newteamId)
+        {    return (
+            <button onClick= {() => fetchTeamSchedule()}> Generate Stats </button>
+        ) }
+        else {
+         return (
+             <button onClick ={()=> backtoGames()}> Return to Today's Games </button>
+        )
+        }
+     }
+     
+           
+
+
     return (
         <div>
             <p>{team.team_name}'s Schedule: </p>
-            {/* <li> {schedule.team.uid}</li> */}
+            {showSchedule(team)}
         </div>
     )
 }
