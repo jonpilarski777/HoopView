11ffd9ee0bd07e338cddf63f9e7fc71a1ec47bab
Jon <jonpilarski927@gmail.com>
2021-12-10 07:35:30 -0600

    fixed nav css

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,23 +1,52 @@
 import React from 'react'
 import {NavLink} from 'react-router-dom'
+import styled from 'styled-components'
 
 function Navbar({handleLogout, currentUser}) {
 
     // const [toggleNavbar, settoggleNavbar] = useState(false)
 
     return (
-        <>
-            <div>
-                <p> Currently signed in as {currentUser.username}</p>
-                <button onClick={handleLogout}>Logout</button>
-            </div>
-            <div>
+        <WholeDiv>
+           
+            <ContentDiv>
                 <NavLink to="/favorite_teams" style={{marginRight: 45}}>Teams</NavLink>
                 <NavLink to="/user_favorite_teams" style={{marginRight: 45}}>Favorite Teams</NavLink>
                 <NavLink to="/games">Games</NavLink>
-            </div>
-        </>
+            </ContentDiv>
+            <NavDiv>
+                <p> Currently signed in as {currentUser.username} <button onClick={handleLogout}>Logout</button></p>
+                
+            </NavDiv>
+        </WholeDiv>
     )
 }
+
+const WholeDiv = styled.div`
+   
+    justify-content: space-between;
+    -webkit-align-items: center;
+    -webkit-box-align: center;
+    -ms-flex-align: center;
+    align-items: center;
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: -ms-flexbox;
+    display: flex;
+    padding: 15px;
+    background-color: #fff; 
+    `
+
+const NavDiv = styled.div`
+    justify-content: center;
+    display: flex;
+    align-items: center;`
+
+
+const ContentDiv = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
 
 export default Navbar
