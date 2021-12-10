68c1ec05b59af8b0933a548198330ab08a153a44
Jon <jonpilarski927@gmail.com>
2021-12-09 18:21:19 -0600

    Grabbing live games - dont forget these are 6 hours ahead/in GMT

--- client/src/components/GamesContainer.js
+++ client/src/components/GamesContainer.js
@@ -1,42 +1,32 @@
-// import React, {useEffect, useState} from 'react'
-// import {Switch, Route} from 'react-router-dom'
-// import GameList from './GameList'
-// import GameDetail from './GameDetail'
-// // import DateTime from './DateTime'
-// // import {getCurrentDate} from './utils'
-// // import Moment from 'react-moment';
-// // import 'moment-timezone';
+import React, {useEffect, useState} from 'react'
+import {Switch, Route} from 'react-router-dom'
+import GameList from './GameList'
+import GameDetail from './GameDetail'
+// import DateTime from './DateTime'
+// import {getCurrentDate} from './utils'
+// import Moment from 'react-moment';
+// import 'moment-timezone';
 
 
 
-// function GamesContainer({currentUser}) {
+function GamesContainer({currentUser}) {
+    const [games, setGames] = useState([])
+    
+  useEffect(() => {fetch('https://www.thesportsdb.com/api/v2/json/50130162/livescore.php?s=Basketball&l=4607')
+  .then(r =>r.json())
+  .then(games => setGames(games))},[])
 
-//     // const [games, setGames] = useState([])
-//     // useEffect((
-
-
-//         // console.log(getCurrentDate())
-
-// //     useEffect(() => {fetch(`http://site.api.espn.com/apis/site/v2/sports/basketball/mens-college-basketball/teams/9`)
-// //     .then(r => r.json())
-// //     .then(games => console.log("games", schedule.team.uid))
-// //     },[])
-// //     //     fetch("/api/daily_games", {
-// //     //         credentials: 'include'
-// //     //     })
-// //     //     .then(r => r.json())
-// //     //     .then(games => setGames(games))
-// //     // }, [])
-
-// //     console.log("games", games)
+  console.log("games:", games)
 
 
 
-//     return (
-//         <div>
-// {/* //             <Moment format="YYYY-MM-DD HH24:MI:SS">
-// //             date_create: moment().format("DD-MM-YYYY hh:mm:ss")
-// //             </Moment> */}
+
+    return (<div> lets gooo </div>)   
+//         (
+       
+                ;// /* //    
+//             date_create: moment().format("DD-MM-YYYY hh:mm:ss")
+// //             </Momentd> */}
 // //             <Switch>
 // //                 <Route
 // //                 exact
@@ -57,9 +47,9 @@
 // //                     )
 // //                 }}               
 //                 />
-//             </Switch>
+// //             </Switch>
 //         </div>
-//     )
-// }
+//     ))
+}
 
-// export default GamesContainer
+export default GamesContainer
